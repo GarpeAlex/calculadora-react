a75e00c0c0d9bba3c2a4980ebded5db14519600a
Alex <garpe.alexalves@gmail.com>
2022-08-31 18:30:42 -0300

    j

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -28,24 +28,24 @@ export default class Calculator extends Component {
     render() {
         return (
             <div className="calculator">
-                <Display value={100} />
-                <Button label="AC" click={this.clearMemory}></Button>
-                <Button label="/" click={this.setOperation}></Button>
+                <Display value={0} />
+                <Button label="AC" click={this.clearMemory} triple></Button>
+                <Button label="/" click={this.setOperation} operation></Button>
                 <Button label="7" click={this.addDigit}></Button>
                 <Button label="8" click={this.addDigit}></Button>
                 <Button label="9" click={this.addDigit}></Button>
-                <Button label="*" click={this.setOperation}></Button>
+                <Button label="*" click={this.setOperation} operation></Button>
                 <Button label="4" click={this.addDigit}></Button>
                 <Button label="5" click={this.addDigit}></Button>
                 <Button label="6" click={this.addDigit}></Button>
-                <Button label="-" click={this.setOperation}></Button>
+                <Button label="-" click={this.setOperation} operation></Button>
                 <Button label="1" click={this.addDigit}></Button>
                 <Button label="2" click={this.addDigit}></Button>
                 <Button label="3" click={this.addDigit}></Button>
-                <Button label="+" click={this.setOperation}></Button>
-                <Button label="0" click={this.addDigit}></Button>
+                <Button label="+" click={this.setOperation} operation></Button>
+                <Button label="0" click={this.addDigit} double></Button>
                 <Button label="." click={this.addDigit}></Button>
-                <Button label="=" click={this.addDigit}></Button>
+                <Button label="=" click={this.addDigit} operation></Button>
             </div>
         );
     }
